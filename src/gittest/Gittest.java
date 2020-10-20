17e49a29f0f125dd75079abb847dad555433276f
Thanatorn <bombenten@hotmail.com>
2020-10-20 15:12:26 +0700

    First Comment

---
+++ src/gittest/Gittest.java
@@ -0,0 +1,12 @@
+package gittest;
+
+public class Gittest {
+
+    public static void main(String[] args) {
+        sayHello();
+    }
+
+    public static void sayHello() {
+        System.out.println("Hello");
+    }
+}
